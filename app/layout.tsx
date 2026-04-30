b378dd3eec3dbf3452c3db19f2fa0b2a50218505
Michał Kwiatkowski <kontakt@kwiatkowskimichal.pl>
2026-04-30 22:58:07 +0200

    scroll final

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="pl"
-      className={`${manrope.variable} ${bricolageGrotesque.variable} h-full antialiased scroll-smooth scroll-pt-20`}
+      className={`${manrope.variable} ${bricolageGrotesque.variable} h-full antialiased scroll-smooth scroll-pt-80`}
     >
       <body className="">
         <Navbar></Navbar>
