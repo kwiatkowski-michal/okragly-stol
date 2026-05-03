e519b8ce538a5c2171b4a33b07cbd4eaa4d9a270
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2026-05-03 13:13:14 +0200

    feat: maskable icon

--- app/manifest.tsx
+++ app/manifest.tsx
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Ogólnopolski Okrągły Stół ds. Strategii Młodzieżowej',
@@ -10,11 +10,11 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#FFFBFB',
     theme_color: '#F2313E',
     icons: [
-        {
+      {
         src: '/icons/512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: "any"
+        purpose: "maskable"
       },
       {
         src: '/icons/192.png',
